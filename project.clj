d429ff5876418e62c6a174b30d50235e668c5ccc
Marc-Elian Bégin <meb@sixsq.com>
2014-06-20 12:09:26 +0200

    Initial development

---
+++ project.clj
@@ -0,0 +1,20 @@
+(defproject lamp "0.1.0-SNAPSHOT"
+  :description "FIXME: write this!"
+  :url "http://example.com/FIXME"
+
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/clojurescript "0.0-2173"]
+                 [org.clojure/core.async "0.1.256.0-1bf8cf-alpha"]]
+
+  :plugins [[lein-cljsbuild "1.0.2"]]
+
+  :source-paths ["src"]
+
+  :cljsbuild { 
+    :builds [{:id "lamp"
+              :source-paths ["src"]
+              :compiler {
+                :output-to "lamp.js"
+                :output-dir "out"
+                :optimizations :none
+                :source-map true}}]})
---
+++ src/lamp/core.cljs
@@ -0,0 +1,72 @@
+(ns lamp.core
+  (:require-macros [cljs.core.async.macros :refer [go]])
+  (:require [goog.dom :as dom]
+            [goog.events :as events]
+            [cljs.core.async :refer [<! put! chan]])
+  (:import [goog.net Jsonp]
+           [goog Uri]))
+
+(def lb-url-read
+  "http://154.48.152.227/mongoRead.php")
+
+(def lb-url-write
+  "http://154.48.152.227/mongoWrite.php")
+
+(defn listen [el type]
+  (let [out (chan)]
+    (events/listen el type
+      (fn [e] (put! out e)))
+    out))
+
+(defn error
+  []
+  (.log js/console "oops"))
+
+(defn jsonp [uri]
+  (let [out (chan)
+        req (Jsonp. (Uri. uri))]
+    (.send req nil (fn [res] (put! out (js->clj res))))
+    out))
+
+(defn render-db
+  [result]
+  (let [db (result "db")]
+    (set!
+      (.-innerHTML
+        (dom/getElement "db"))
+      (str "Database instance: " db))))
+
+(defn render-read
+  [result]
+  (let [writers (result "writers")
+        sorted (sort writers)
+        filtered (filter #(> (val %) 0) writers)]
+    (render-db result)
+    (set!
+      (.-innerHTML
+        (dom/getElement "writers"))
+      (apply str
+        (for [[k v] filtered]
+          (str "<li>" k ": " v "</li>"))))))
+
+(defn render-write
+  [result]
+  (render-db result)
+  (set!
+    (.-innerHTML
+      (dom/getElement "writers"))
+    ""))
+
+(defn init []
+  (let [read (listen (dom/getElement "read") "click")
+        write (listen (dom/getElement "write") "click")]
+    (go (while true
+          (<! read)
+          (let [result (<! (jsonp lb-url-read))]
+            (render-read result))))
+    (go (while true
+          (<! write)
+          (let [result (<! (jsonp lb-url-write))]
+            (render-write result))))))
+
+(init)
